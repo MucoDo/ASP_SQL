c0b8745a5e793ac76b0a29924cf1b60ea4634f9b
Dorine Muco <muco.dorine@gmail.com>
2024-02-22 21:02:23 +0100

    Ecoscore ne s'affiche pas correctement

--- ASP_EcoResp/Models/SearchHomeForm.cs
+++ ASP_EcoResp/Models/SearchHomeForm.cs
@@ -1,7 +1,13 @@
-﻿namespace ASP_EcoResp.Models
+﻿using BLL_EcoResp.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_EcoResp.Models
 {
     public class SearchHomeForm
     {
+        [DisplayName("Recherche par nom ou par catégorie")]
         public string Search {  get; set; }
+ 
     }
 }
