6418a873420c69931b4c54647f08d94aa0780322
Dorine Muco <muco.dorine@gmail.com>
2024-02-22 11:40:02 +0100

    delete cascade tables ProduitCommande et Media

--- Database/Tables/Media.sql
+++ Database/Tables/Media.sql
@@ -3,5 +3,5 @@
 	[Id_Media] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
     [LienMedia] NVARCHAR(256) NOT NULL, 
     [Id_Produit] INT NOT NULL, 
-    CONSTRAINT [FK_Media_Produit] FOREIGN KEY ([Id_Produit]) REFERENCES [Produit]([Id_Produit])
+    CONSTRAINT [FK_Media_Produit] FOREIGN KEY ([Id_Produit]) REFERENCES [Produit]([Id_Produit]) ON DELETE CASCADE
 )
--- Database/Tables/ProduitCommande.sql
+++ Database/Tables/ProduitCommande.sql
@@ -4,6 +4,6 @@
     [Quantité] INT NOT NULL, 
     [Id_Produit] INT NOT NULL, 
     [Id_Commande] INT NOT NULL, 
-    CONSTRAINT [FK_ProduitCommande_Produit] FOREIGN KEY ([Id_Produit]) REFERENCES [Produit]([Id_Produit]), 
+    CONSTRAINT [FK_ProduitCommande_Produit] FOREIGN KEY ([Id_Produit]) REFERENCES [Produit]([Id_Produit])  ON DELETE CASCADE, 
     CONSTRAINT [FK_ProduitCommande_Commande] FOREIGN KEY ([Id_Commande]) REFERENCES [Commande]([Id_Commande])
 )
